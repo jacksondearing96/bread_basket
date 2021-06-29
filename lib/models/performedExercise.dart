4e0d79bbc1524939db6cca7947614ea3033867e3
Jackson Dearing <jacksondearing96@gmail.com>
2021-06-29 14:50:19 +1000

    Applied sorting to exercises within a workout when JSON is parsed.

--- lib/models/performedExercise.dart
+++ lib/models/performedExercise.dart
@@ -9,26 +9,20 @@ class PerformedExercise {
   PerformedExercise({required this.exercise});
 
   static PerformedExercise fromJson(Map<String, Object?> json, String id) {
-    print('Creating a PerformedExercise from json, using: ');
-    print(json);
-    print('id: ${json['exerciseId']! as String}');
-    print('name: ${json['name']! as String}');
-    print('tags: ${json['tags']! as List<dynamic>}');
     PerformedExercise performedExercise = PerformedExercise(
         exercise: Exercise(
             id: json['exerciseId']! as String,
             name: json['name']! as String,
             tags: json['tags']! as List<dynamic>));
     performedExercise.id = id;
-    print('progress: ');
-    print(performedExercise.id);
 
     Map<String, Object?> setsJson = json['sets']! as Map<String, Object?>;
-    print('setsJson: ${setsJson}');
     for (var setId in setsJson.keys) {
       performedExercise.sets.add(PerformedSet.fromJson(
           setsJson[setId]! as Map<String, dynamic>, setId));
     }
+    performedExercise.sets
+        .sort((a, b) => int.parse(a.id).compareTo(int.parse(b.id)));
 
     return performedExercise;
   }
