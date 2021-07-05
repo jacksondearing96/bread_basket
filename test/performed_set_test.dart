718328f6372d4694175a7e74edba42639badd1ac
Jackson Dearing <jacksondearing96@gmail.com>
2021-07-05 12:07:39 +1000

    Added basic unit testing files.

---
+++ test/performed_set_test.dart
@@ -0,0 +1,42 @@
+import 'package:bread_basket/models/performedSet.dart';
+import 'package:bread_basket/shared/constants.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Performed Set Tests', () {
+    late PerformedSet performedSet;
+    const double testWeight = 7.0;
+    const int testReps = 8;
+
+    setUp(() {
+      performedSet = PerformedSet();
+      performedSet.weight = testWeight;
+      performedSet.reps = testReps;
+    });
+
+    test('Initialisation', () {
+      PerformedSet initialisedSet = PerformedSet();
+      expect(initialisedSet.setType, Constants.normalCode);
+      expect(initialisedSet.reps, 0);
+      expect(initialisedSet.weight, 0.0);
+    });
+
+    test('Get weight string', () {
+      expect(performedSet.getWeightString(), '7');
+
+      performedSet.weight = 10.1;
+      expect(performedSet.getWeightString(), '10.1');
+    });
+
+    test('Volume', () {
+      expect(performedSet.volume(), 56.0);
+    });
+
+    test('ToJson', () {
+      expect(
+        performedSet.toJson(),
+        {'type': Constants.normalCode, 'weight': testWeight, 'reps': testReps}
+      );
+    });
+  });
+}
---
+++ test/sign_in_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Sign In', () {
+    test('Email validation', () {
+
+      expect(true, true);
+    });
+  });
+}
