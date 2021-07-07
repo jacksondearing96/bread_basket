ac5e982bcc6609d1024e529b6929e2f82b37ca15
Jackson Dearing <jacksondearing96@gmail.com>
2021-07-07 15:27:54 +1000

    Added screenshot to failing test to help debugging.

--- test/widgets/sign_in_test.dart
+++ test/widgets/sign_in_test.dart
@@ -1,7 +1,6 @@
 import 'package:bread_basket/screens/authenticate/signIn.dart';
 import 'package:bread_basket/services/auth.dart';
 import 'package:bread_basket/shared/keys.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
@@ -21,8 +20,10 @@ void main() {
     signIn = !signIn;
   }
 
-  testWidgets('Sign in widget: email input text field',
+  testGoldens('Sign in widget: email input text field',
       (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = Size(1284, 2778);
+    addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
     await tester.pumpWidget(MaterialApp(
         home: SignIn(toggleView: toggleView, authService: fakeAuthService)));
 
@@ -30,6 +31,8 @@ void main() {
     final passwordField = find.byKey(Keys.signInPasswordField);
     final signInButton = find.byKey(Keys.signInButton);
     var errorMessage = find.byKey(Keys.signInErrorMessage);
+    await expectLater(
+        find.byType(MaterialApp), matchesGoldenFile('signIn.png'));
     expect(emailField, findsOneWidget);
     expect(passwordField, findsOneWidget);
     expect(signInButton, findsOneWidget);
