f14bd922d0a0344a781476244837d86dcc8fa504
Jackson Dearing <jacksondearing96@gmail.com>
2021-07-24 13:46:45 +1000

    Scaled pie chart size relative to the available size on the screen.

--- lib/analytics/MuscleGroupPieChart.dart
+++ lib/analytics/MuscleGroupPieChart.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bread_basket/services/history.dart';
 import 'package:bread_basket/shared/constants.dart';
 import 'package:flutter/material.dart';
@@ -30,7 +32,7 @@ class _MuscleGroupPieChartState extends State<MuscleGroupPieChart> {
         children: [
           Text('Muscle group relative distribution',
               style: TextStyle(color: Constants.hintColor)),
-              SizedBox(height: 10),
+          SizedBox(height: 10),
           Row(
             children: <Widget>[
               Expanded(
@@ -38,30 +40,34 @@ class _MuscleGroupPieChartState extends State<MuscleGroupPieChart> {
                   aspectRatio: 1,
                   child: Padding(
                     padding: EdgeInsets.all(10),
-                    child: PieChart(
-                      PieChartData(
-                          pieTouchData:
-                              PieTouchData(touchCallback: (pieTouchResponse) {
-                            setState(() {
-                              final desiredTouch = pieTouchResponse.touchInput
-                                      is! PointerExitEvent &&
-                                  pieTouchResponse.touchInput is! PointerUpEvent;
-                              if (desiredTouch &&
-                                  pieTouchResponse.touchedSection != null) {
-                                touchedIndex = pieTouchResponse
-                                    .touchedSection!.touchedSectionIndex;
-                              } else {
-                                touchedIndex = -1;
-                              }
-                            });
-                          }),
-                          borderData: FlBorderData(
-                            show: false,
-                          ),
-                          sectionsSpace: 0,
-                          centerSpaceRadius: 40,
-                          sections: showingSections()),
-                    ),
+                    child: LayoutBuilder(
+                        builder: (context, BoxConstraints constraints) {
+                      return PieChart(
+                        PieChartData(
+                            pieTouchData:
+                                PieTouchData(touchCallback: (pieTouchResponse) {
+                              setState(() {
+                                final desiredTouch = pieTouchResponse.touchInput
+                                        is! PointerExitEvent &&
+                                    pieTouchResponse.touchInput
+                                        is! PointerUpEvent;
+                                if (desiredTouch &&
+                                    pieTouchResponse.touchedSection != null) {
+                                  touchedIndex = pieTouchResponse
+                                      .touchedSection!.touchedSectionIndex;
+                                } else {
+                                  touchedIndex = -1;
+                                }
+                              });
+                            }),
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            sectionsSpace: 0,
+                            centerSpaceRadius: 40,
+                            sections: showingSections(constraints.maxWidth)),
+                      );
+                    }),
                   ),
                 ),
               ),
@@ -102,7 +108,7 @@ class _MuscleGroupPieChartState extends State<MuscleGroupPieChart> {
     return labels;
   }
 
-  List<PieChartSectionData> showingSections() {
+  List<PieChartSectionData> showingSections(double maxWidth) {
     List<Color> colors = []..addAll(Constants.pieChartColors);
     return List.generate(Constants.muscleGroups.length, (i) {
       final muscleGroup = Constants.muscleGroups[i];
@@ -110,10 +116,9 @@ class _MuscleGroupPieChartState extends State<MuscleGroupPieChart> {
       final exerciseCount = muscleGroupToExerciseCount.containsKey(muscleGroup)
           ? muscleGroupToExerciseCount[muscleGroup]!.toDouble()
           : 0.0;
-      // final proportionOfTotalExercises =
-      //     (totalExerciseCount == 0) ? 0.0 : exerciseCount / totalExerciseCount;
-      final fontSize = isTouched ? 18.0 : 12.0;
-      final radius = isTouched ? 80.0 : 60.0;
+      final fontSize = isTouched ? maxWidth * 0.1 : maxWidth * 0.05;
+      final radius = isTouched ? maxWidth * 0.4 : maxWidth * 0.3;
+      print(maxWidth);
       Color color = Colors.grey;
       if (exerciseCount > 0) {
         color = colors.removeAt(0);
